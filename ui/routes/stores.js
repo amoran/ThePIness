24c44775fa0d97e2663fd9a8c763367da7f2e3d8
Adam Moran <amoran@andrew.cmu.edu>
2016-07-20 16:15:03 -0500

    Remix routes

--- ui/routes/stores.js
+++ ui/routes/stores.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/', function(req,res) {
+router.get('/', function(req,res) {
 		res.json({ message: 'API is running. Use /api/{route} to access it.' });
 });
 
