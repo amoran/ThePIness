bcdab419942da0089c19e6f74fba04d090b375cd
Adam Moran <amorancs@gmail.com>
2016-07-20 22:04:16 +0000

    Install mysql and test connection

--- ui/routes/stores.js
+++ ui/routes/stores.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'mysql123',
+	database: 'pizza'
+});
+
+connection.connect();
+
+function endConnection() {
+	connection.end();
+}
 
 router.get('/', function(req,res) {
-		res.json({ message: 'API is running. Use /api/{route} to access it.' });
+
+
+	var result = 'oops';
+	connection.query('SELECT * FROM STORES', function(err, rows, fields) {
+		if (err) throw err;
+		result = rows[0];
+		console.log('The solution is: ', rows[0]);
+		res.json({ stores: 'Result: '+ JSON.stringify(result) });
+	});
+
 });
 
 module.exports = router;
